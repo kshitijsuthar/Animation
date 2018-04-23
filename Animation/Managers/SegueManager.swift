b646244ab9da2e0cc11c9f1b1111da3b4f338267
Kshitij Suthar <kshitijsuthar@kshitijs-macbook-pro.local>
2018-04-23 19:54:26 -0400

    Added animations on StartViewController

---
+++ Animation/Managers/SegueManager.swift
@@ -0,0 +1,24 @@
+//
+//  SegueManager.swift
+//  Animation
+//
+//  Created by Kshitij Suthar on 2018-04-23.
+//  Copyright © 2018 Kshitij Suthar. All rights reserved.
+//
+
+import Foundation
+
+class SegueManager {
+    
+    class var StartView:String {
+        return "StartView"
+    }
+    
+    class var SecondView:String {
+        return "SecondView"
+    }
+    
+    class var EndView:String {
+        return "EndView"
+    }
+}
--- Animation/View Controllers/StartViewController.swift
+++ Animation/View Controllers/StartViewController.swift
@@ -14,18 +14,39 @@ class StartViewController: UIViewController {
 
     override func viewWillAppear(_ animated: Bool) {
         StartLabel.alpha = 0
-        //NextButton.isHidden = true
+        NextButton.isHidden = true
         
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        UIView.animate(withDuration: 1) {
-            self.StartLabel.alpha = 1
-        }
+       
+        AnimateLabel()
+        AnimateButtonIn()
+        MoveButtonOver()
+       
     }
-
+    
     @IBAction func NextButton(_ sender: Any) {
         
         performSegue(withIdentifier: SegueManager.SecondView, sender: sender)
+    }
+    
+    func AnimateLabel() {
+        UIView.animate(withDuration: 1, delay: 0.25, options: [.curveEaseInOut], animations: {
+            self.StartLabel.alpha = 1
+            self.StartLabel.center.y -= 50
+        }, completion: nil)
+    }
+    
+    func AnimateButtonIn() {
+        UIView.transition(with: self.NextButton, duration: 2, options: [.transitionFlipFromBottom], animations: {
+            self.NextButton.isHidden = false
+        }, completion: nil)
+    }
+    
+    func MoveButtonOver() {
+        UIView.animate(withDuration: 2, delay: 0.25, options: [.curveEaseIn], animations: {
+            self.NextButton.center.x += 100
+        }, completion: nil)
     }
 }
